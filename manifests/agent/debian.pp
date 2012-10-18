333d647362647fb96879a5a7e898cbfa712363e7
Scott Schneider <sschneider@puppetlabs.com>
2012-10-18 09:38:29 -0700

    Install Java, and not all of that other stuff (for now)

--- manifests/agent/debian.pp
+++ manifests/agent/debian.pp
@@ -1,27 +1,7 @@
 class jenkins::agent::debian {
   package {
     [
-      'autoconf',
-      'automake',
-      'bison',
-      'build-essential',
-      'curl',
-      'git-core',
-      'libc6-dev',
-      'libncurses5-dev',
-      'libreadline5',
-      'libreadline6-dev',
-      'libsqlite3-dev',
-      'libssl-dev',
-      'libtool',
-      'libxml2-dev',
-      'libxslt-dev',
-      'libyaml-dev',
-      'openssl',
-      'sqlite3',
-      'subversion',
-      'zlib1g',
-      'zlib1g-dev',
+      'default-jre-headless',
     ]:
       ensure => 'installed',
   }
