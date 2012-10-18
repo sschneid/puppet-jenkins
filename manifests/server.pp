ceaf7f97c266581287822d21cb5cf91a9aae0eb7
Scott Schneider <sschneider@puppetlabs.com>
2012-10-18 16:38:10 -0700

    Collect associated agents

--- manifests/server.pp
+++ manifests/server.pp
@@ -21,6 +21,13 @@ class jenkins::server (
       site_alias => $real_site_alias,
   }
 
+
+  # Collect agents associated with this server
+  Jenkins_agent <<| server == $real_site_alias |>>
+  if ($real_site_alias != $::fqdn) {
+    Jenkins_agent <<| server == $::fqdn |>>
+  }
+
   Class['jenkins::repo'] ->
   Class['jenkins::package'] ->
   Class['jenkins::service'] ->
