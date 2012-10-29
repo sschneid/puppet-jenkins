e39dff8dd81e36a14abb63d12b679dd79c8e4ec8
Scott Schneider <sschneider@puppetlabs.com>
2012-10-29 10:32:35 -0700

    Allow the rest of the built-in params

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,11 +1,31 @@
 class jenkins::agent (
-  $labels = undef,
-  $server = undef,
+  $name      = undef,
+  $server    = undef,
+
+  $username  = undef,
+  $password  = undef,
+
+  $executors = undef,
+  $launcher  = undef,
+  $homedir   = undef,
+  $ssh_user  = undef,
+  $ssh_key   = undef,
+  $labels    = undef,
 ) inherits jenkins {
   if ($server) {
     @@jenkins_agent { $::fqdn:
-      labels => $labels,
-      server => $server,
+      name      => $name,
+      server    => $server,
+
+      username  => $username,
+      password  => $password,
+
+      executors => $executors,
+      launcher  => $launcher,
+      homedir   => $homedir,
+      ssh_user  => $ssh_user,
+      ssh_key   => $ssh_key,
+      labels    => $labels,
     }
   }
 
